66282e986efbd5b614208262c104718e922452ec
straviaz <joy.sangsrichan@gmail.com>
2021-01-05 00:44:52 -0500

    answered 7 questions

--- data.py
+++ data.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import numpy as np 
+import matplotlib.pyplot as plt
+from IPython.display import display, HTML, display_html
 
-def parse_world_happiness_file(path):
-    fp = open(path, 'r')
-    lines = fp.readlines()
+def convert_csv_to_df(path):
+    df = pd.read_csv(path, header=0)
+    return df
 
-    for line in lines:
-        print(line)
+def construct_dataframe():
+    df_happiness = convert_csv_to_df('datasets/world-happiness-report/2020.csv')
+    df_countries = convert_csv_to_df('datasets/countries of the world.csv')
+    merged_df = pd.merge(df_happiness, df_countries, how='inner', on='Country')
+    merged_df.sort_values(by=['Rank'])
+    merged_df.to_csv(r'datasets/output.csv')
+    return merged_df
 
-def main():
-    parse_world_happiness_file('/Users/joy/Desktop/Repositories/world-happiness-report-analysis/datasets/world-happiness-report/2020.csv')
-
-if __name__=="__main__": 
-    main() 
+def display_side_by_side(*args):
+    html_str=''
+    for df in args:
+        html_str+=df.to_html(index=False)
+    display_html(html_str.replace('table','table style="display:inline"'),raw=True)
